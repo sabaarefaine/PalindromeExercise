2cc691ab9f53bd7e2f9118259a7ae9e76f09a19f
Saba Arefaine <arefainesaba@gmail.com>
2021-04-25 10:44:05 -0700

    palindrome tester

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        //create a method(IsAPalindrome) that will take a parameter of type string
+        //and return a value of type bool.
+        //If the given input is a palindrome – return true, else return false. 
+
+        public bool IsAPalindrome(string value)
+        {
+            string reverse = new string(value.Reverse().ToArray());
+
+            return string.Compare(value, reverse) == 0 ? true : false;
+        }
+
+       
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,28 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("hannah", true)]
+        public void MyTest(string value, bool expected)
         {
+            //arrange
 
+            var test = new WordSmith();
+
+            //act
+
+            bool actual = test.IsAPalindrome(value);
+
+            //assert
+
+            Assert.Equal(expected, actual);
         }
     }
 }
